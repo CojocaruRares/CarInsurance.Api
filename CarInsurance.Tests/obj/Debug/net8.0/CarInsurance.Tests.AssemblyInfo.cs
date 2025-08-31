4912093d3b5f3485db8869e1b2a014ca3c250a54
root <root@DESKTOP-GHQI9QT.localdomain>
2025-08-31 12:04:53 +0300

    unit test

--- CarInsurance.Tests/obj/Debug/net8.0/CarInsurance.Tests.AssemblyInfo.cs
+++ CarInsurance.Tests/obj/Debug/net8.0/CarInsurance.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarInsurance.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df128e6954372e05a7903cedb9bdc95e367086dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e8a71688debfc4e146d39eccb0d8bf2e923ac9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarInsurance.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarInsurance.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
